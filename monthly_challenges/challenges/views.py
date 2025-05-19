810341566ec3c1558a29168a00d86d2626fc18f9
Masoud <masoudmashayekh4944@gmial.com>
2025-05-19 13:37:55 +0200

    19.may

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This works!")
+def january(request):
+    return HttpResponse("This works! 1th Month")
+
+def february(request):
+    return HttpResponse("2th Month")
