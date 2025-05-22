90638c6096355702e044f7d34a66d4a87957ec76
Masoud <masoudmashayekh4944@gmail.com>
2025-05-22 09:10:13 +0200

    make shorter views.py with deleting render_to_string

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
+
 
 monthly_challenges_dic = {
     "january": "1. Build a calculator that supports basic operations (+, -, *, /) and handles errors gracefully.",
@@ -47,7 +47,6 @@ def monthly_challenges_by_number(request, month):
 def monthly_challenges_by_text(request, month):
     try:
         challenge_text = monthly_challenges_dic[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html")
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
