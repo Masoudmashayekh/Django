18382a0721431b47207bf2b56df3fc3361a7fcf4
Masoud <masoudmashayekh4944@gmail.com>
2025-05-24 16:33:43 +0200

    add 404.html and including partial template

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-
+from django.template.loader import render_to_string
 
 monthly_challenges_dic = {
     "january": "1. Build a calculator that supports basic operations (+, -, *, /) and handles errors gracefully.",
@@ -55,5 +55,6 @@ def monthly_challenges_by_text(request, month):
             })
     
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
  
