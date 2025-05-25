f8f49d441a86a4698c906ebf227a82355b8123cd
Masoud <masoudmashayekh4944@gmail.com>
2025-05-25 16:37:18 +0200

    wire up urls.py and views.py in the main urls.py

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.starting_page),
-    path("posts",views.posts),
-    path("posts/<slug:slug>",views.post_detail)  # /posts/my-first-post
+    path("",views.starting_page, name="starting-page"),
+    path("posts",views.posts, name="posts-page"),
+    path("posts/<slug:slug>",views.post_detail, name= "post-detail-page")  # /posts/my-first-post
 ]
 
