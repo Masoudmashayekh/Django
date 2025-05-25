8c8aebbd0831c78f49c6a8448be605c2aaa3eafe
Masoud <masoudmashayekh4944@gmail.com>
2025-05-25 16:13:55 +0200

    wire up views.pu with urls.py

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path(""),
-    path("posts"),
-    path("posts/<slug:slug>")  # /posts/my-first-post
+    path("",views.starting_page),
+    path("posts",views.posts),
+    path("posts/<slug:slug>",views.post_detail)  # /posts/my-first-post
 ]
 
