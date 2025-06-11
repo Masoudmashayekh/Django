8b7c1d1174ac524081cb30459a630927a3eb8e45
Masoud <masoudmashayekh4944@gmail.com>
2025-06-11 11:57:00 +0200

    edit admin part and start section 8

--- BOOK_STORE/book_outlet/models.py
+++ BOOK_STORE/book_outlet/models.py
@@ -1,19 +1,21 @@
 from django.db import models
-from django.core.validators import MinLengthValidator,MaxLengthValidator
+from django.core.validators import MinValueValidator,MaxValueValidator
 from django.utils.text import slugify
-
+from django.urls import reverse
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinLengthValidator(1),MaxLengthValidator(5)])
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
     author = models.CharField(null=True ,max_length=100)
     is_bestselling = models.BooleanField(default=False)
     # Harry Potter 1 => harry-potter-1
-    slug = models.SlugField(default="",blank=True,editable=False, null=False)
+    slug = models.SlugField(default="", null=False)
     
     
+    def get_absolute_url(self):
+        return reverse("book-detial", args=[self.slug])
     
     
     
