177a0b16f808809621e33e8c3bd0f4fd7807807c
Masoud <masoudmashayekh4944@gmail.com>
2025-06-30 21:24:54 +0200

    working with the file field

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from .forms import ProfileForm
 from .models import UserProfile
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 # Create your views here.
 # def store_file(file):
@@ -32,3 +33,9 @@ class CreateProfileView(CreateView):
 #             return HttpResponseRedirect("/profiles")
         
 #         return render(request, "profiles/create_profile.html", {"form": submitted_form})
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
+    
