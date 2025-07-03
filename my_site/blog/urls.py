126cfac0dceab0fcf64013f6541a1b2c47cc9a6a
Masoud <masoudmashayekh4944@gmail.com>
2025-07-03 20:17:25 +0200

    add listview

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",views.StartingPageView.as_view(), name="starting-page"),
-    path("posts",views.posts, name="posts-page"),
+    path("posts",views.AllPostView.as_view(), name="posts-page"),
     path("posts/<slug:slug>",views.post_detail, name= "post-detail-page")  # /posts/my-first-post
 ]
 
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -19,9 +19,16 @@ class StartingPageView(ListView):
 #     latest_posts = Post.objects.all().order_by("-date")[:3]
 #     return render(request, "blog/index.html",{"posts": latest_posts})
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html",{"all_posts": all_posts})
+
+class AllPostView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+    
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html",{"all_posts": all_posts})
    
 
 def post_detail(request,slug):
