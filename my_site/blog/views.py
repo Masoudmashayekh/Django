c232ee5df5bb6ebee40f2dbade68b1997067e84b
Masoud <masoudmashayekh4944@gmail.com>
2025-07-04 19:23:38 +0200

    handling comment form submission

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView
 from .forms import CommentForm
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 class StartingPageView(ListView):
@@ -32,15 +35,46 @@ class AllPostView(ListView):
 #     return render(request, "blog/all-posts.html",{"all_posts": all_posts})
    
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+class SinglePostView(View):
+    def get(self, request, slug):
+        post= Post.objects.get(slug= slug)
+        context={
+            "post": post,
+            "post-tags": post.tags.all(),
+            "comment-form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.Post)
+        post = Post.objects.get(slug= slug)
+        
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context={
+            "post": post,
+            "post-tags": post.all(),
+            "comment-form": comment_form
+        }
+        return render(request,"blog/post-detail-page.html", context)
+        
+
+
+
+# class SinglePostView(DetailView):
+#     template_name = "blog/post-detail.html"
+#     model = Post
+
+#     def get_context_data(self, **kwargs):
+#         context =  super().get_context_data(**kwargs)
+#         context["post_tags"] = self.object.tags.all()
+#         context["comment_form"] = CommentForm()
+#         return context
 
 # def post_detail(request,slug):
 #     identified_post = get_object_or_404(Post, slug = slug)
